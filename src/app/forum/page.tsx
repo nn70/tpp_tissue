503582f486d3445e21d7c01c5429a4a2d4800e1b
nn70 <nn4work@gmail.com>
2026-09-05 22:59:24 +0800

    Use internal volunteer registration page

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
 import RestrictedAccess from "@/components/RestrictedAccess";
+import VolunteerForumClient from "@/components/VolunteerForumClient";
 
 export default async function ForumPage() {
     const session = await getServerSession(authOptions);
@@ -10,14 +10,5 @@ export default async function ForumPage() {
         return <RestrictedAccess />;
     }
 
-    return (
-        <div className="h-screen w-full pt-16 flex flex-col bg-[#0f1016]">
-            <iframe
-                src="https://registerforum.vercel.app/"
-                className="w-full h-full border-none flex-1"
-                title="志工報名系統(可追蹤)"
-                allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
-            />
-        </div>
-    );
+    return <VolunteerForumClient />;
 }
